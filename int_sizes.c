ea406b41dbcd0a851bacfe67b1ebf555aefe51b8
Simon Bertling <simon.bertling@gmx.de>
2018-01-28 22:59:42 +0900

    added more types

--- int_sizes.c
+++ int_sizes.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
+#include <stdint.h>
 
 int main()
 {
-	char c;
-	short s;
-	int i;
-	long l;
-	long int li;
-	long long int lli;
-
-	printf("sizeof(char) = %d\n", sizeof(c));
-	printf("sizeof(short) = %d\n", sizeof(s));
-	printf("sizeof(int) = %d\n", sizeof(i));
-	printf("sizeof(long) = %d\n", sizeof(l));
-	printf("sizeof(long int) = %d\n", sizeof(li));
-	printf("sizeof(long long int) = %d\n", sizeof(lli));
+	printf("sizeof(char) = %d\n", (int)sizeof(char));
+	printf("sizeof(short) = %d\n", (int)sizeof(short));
+	printf("sizeof(int) = %d\n", (int)sizeof(int));
+	printf("sizeof(long) = %d\n", (int)sizeof(long));
+	printf("sizeof(long int) = %d\n", (int)sizeof(long int));
+	printf("sizeof(long long int) = %d\n", (int)sizeof(long long int));
+	printf("\n");
+	printf("sizeof(int8_t) = %d\n", (int)sizeof(int8_t));
+	printf("sizeof(int16_t) = %d\n", (int)sizeof(int16_t));
+	printf("sizeof(int32_t) = %d\n", (int)sizeof(int32_t));
+	printf("sizeof(int64_t) = %d\n", (int)sizeof(int64_t));
+	printf("\n");
+	printf("sizeof(int_fast8_t) = %d\n", (int)sizeof(int_fast8_t));
+	printf("sizeof(int_fast16_t) = %d\n", (int)sizeof(int_fast16_t));
+	printf("sizeof(int_fast32_t) = %d\n", (int)sizeof(int_fast32_t));
+	printf("sizeof(int_fast64_t) = %d\n", (int)sizeof(int_fast64_t));
+	printf("\n");
+	printf("sizeof(float) = %d\n", (int)sizeof(float));
+	printf("sizeof(double) = %d\n", (int)sizeof(double));
 }
